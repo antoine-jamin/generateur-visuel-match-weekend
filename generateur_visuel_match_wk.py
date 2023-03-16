496dcfae63921779a4d666d158feb259e40f9a53
Antoine JAMIN <antoine-jamin@users.noreply.github.com>
2023-03-16 22:49:39 +0100

    amélioration chgt taille et espacement

--- generateur_visuel_match_wk.py
+++ generateur_visuel_match_wk.py
@@ -13,7 +13,7 @@ salles_equipes_conventions = {'JEAN MONNET': ['S1F', 'S2F', 'S3F', 'S4F', 'U17F'
                               'BELLE BEILLE': ['S1F', 'S2F', 'S3F', 'S4F', 'U17F'],
                               'MONTAIGNE': ['U15M', 'U16M1']}
 
-date_jour1 = datetime.date(2023, 2, 4)
+date_jour1 = datetime.date(2023, 3, 18)
 
 xl_matchs = pd.read_excel('doc/template.xlsx').dropna().sort_values(by=['JOUR', 'HEURE'])
 
@@ -21,12 +21,19 @@ y_match = 90
 y = 200
 template_img = Image.open('doc/template.png')
 visuel = ImageDraw.Draw(template_img)
+# FONTS
 font_jour = ImageFont.truetype("Chalkboard.ttc", 60)
 #font_jour = ImageFont.truetype("Chalkboard.ttc", 50)
 font_salle = ImageFont.truetype("Chalkboard.ttc", 45)
 #font_salle = ImageFont.truetype("Chalkboard.ttc", 40)
 font_match = ImageFont.truetype("Chalkboard.ttc", 40)
 #font_match = ImageFont.truetype("Chalkboard.ttc", 35)
+# ESPACEMENTS
+delta_h_jour = 30
+#delta_h_jour = 22
+delta_h_lieu = 25
+#delta_h_lieu = 17
+# COULEURS
 color_jour = (254, 253, 26, 255)
 color_salle_conv = (0, 176, 240, 255)
 color_salle_ext = (255, 11, 172, 255)
@@ -40,7 +47,7 @@ for ij, jour in enumerate(jours):
         w, h = visuel.textsize(msg, font_jour)
         visuel.text(((1080 - w) / 2, y), msg, font=font_jour, fill=color_jour, align='center', stroke_width=2,
                     stroke_fill=color_jour)
-        y += h + 30
+        y += h + delta_h_jour
 
     # DOMICILE
     index_to_rm = []
@@ -53,7 +60,7 @@ for ij, jour in enumerate(jours):
                 w, h = visuel.textsize(salle, font_salle)
                 visuel.text(((1080 - w) / 2, y), salle, font=font_salle, fill=color_salle_pdc, stroke_width=1,
                             stroke_fill=color_salle_pdc)
-                y += h + 25
+                y += h + delta_h_lieu
             heure = row['HEURE'].split("H")[0]
             if row['HEURE'].upper().split("H")[1] != '':
                 heure = row['HEURE'].upper()
@@ -70,7 +77,7 @@ for ij, jour in enumerate(jours):
             msg = msg[:-1]
             w, h = visuel.textsize(msg.expandtabs(), font_match)
             visuel.multiline_text((y_match, y), msg.expandtabs(), font=font_match, fill=color_match)
-            y += h + 25
+            y += h + delta_h_lieu
     # CONVENTION
     for i, salle in enumerate(salles_equipes_conventions):
         msg = ""
@@ -83,7 +90,7 @@ for ij, jour in enumerate(jours):
                 w, h = visuel.textsize(salle, font_salle)
                 visuel.text(((1080 - w) / 2, y), salle, font=font_salle, fill=color_salle_conv, stroke_width=1,
                             stroke_fill=color_salle_conv)
-                y += h + 25
+                y += h + delta_h_lieu
             heure = row['HEURE'].split("H")[0]
             if row['HEURE'].upper().split("H")[1] != '':
                 heure = row['HEURE'].upper()
@@ -98,7 +105,7 @@ for ij, jour in enumerate(jours):
             msg = msg[:-1]
             w, h = visuel.textsize(msg.expandtabs(), font_match)
             visuel.multiline_text((y_match, y), msg.expandtabs(), font=font_match, fill=color_match)
-            y += h + 25
+            y += h + delta_h_lieu
         cpt += 1
     # EXTERIEUR
     msg = ""
@@ -109,7 +116,7 @@ for ij, jour in enumerate(jours):
                 w, h = visuel.textsize("EXTÉRIEUR", font_salle)
                 visuel.text(((1080 - w) / 2, y), "EXTÉRIEUR", font=font_salle, fill=color_salle_ext, stroke_width=1,
                             stroke_fill=color_salle_ext)
-                y += h + 25
+                y += h + delta_h_lieu
             heure = row['HEURE'].split("H")[0]
             if row['HEURE'].upper().split("H")[1] != '':
                 heure = row['HEURE'].upper()
@@ -124,7 +131,7 @@ for ij, jour in enumerate(jours):
         msg = msg[:-1]
         w, h = visuel.textsize(msg.expandtabs(), font_match)
         visuel.multiline_text((y_match, y), msg.expandtabs(), font=font_match, fill=color_match)
-        y += h + 25
+        y += h + delta_h_lieu
 
 name_file = "visuel_wk_" + date_jour1.strftime("%Y%m")
 for n, j in enumerate(jours):
